eafd3d349c96e9d1f293d4c08996c562d9b3fef0
Pediah <131521902+Pediah@users.noreply.github.com>
2024-01-26 21:02:33 +0200

    js to new

--- script.js
+++ script.js
@@ -37,3 +37,31 @@ document.addEventListener("DOMContentLoaded", function () {
         revealObserver.observe(element);
     });
 });
+// JavaScript for toggle menu
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
+
+// JavaScript for showing words one after the other
+document.addEventListener("DOMContentLoaded", function() {
+    const words = document.querySelectorAll('.word');
+    let index = 0;
+
+    function showWord() {
+        if (index < words.length) {
+            words[index].style.opacity = '1';
+            index++;
+            setTimeout(showWord, 1000); // Adjust the delay as needed (in milliseconds)
+        }
+    }
+
+    showWord(); // Start the animation
+});
+
+
